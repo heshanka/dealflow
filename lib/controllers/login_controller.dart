68b639b0f81d56276802096d493a5a3b59d6616a
heshanka <info@heshanka.com>
2022-03-13 22:41:53 +0300

    Added login conveyor and login controller

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,53 @@
+
+import 'dart:async';
+
+import 'package:dealflow_coding_assignment/conveyors/login_conveyor.dart';
+import 'package:flutter/foundation.dart';
+
+class LoginController with ChangeNotifier {
+  bool _isSuccess = false;
+  bool _isLoading = false;
+  bool showPassword = false;
+  static bool isUserLoggedIn = false;
+  bool get isSuccess => _isSuccess;
+
+  bool get isLoading => _isLoading;
+
+  void changeLoginStatus(bool status) {
+    isUserLoggedIn = status;
+    notifyListeners();
+  }
+
+  bool isEmail(String str) {
+    if (str == null || str.trim().isEmpty) return false;
+    RegExp emailRegex = RegExp(
+        r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$");
+
+    return emailRegex.hasMatch(str.trim().toLowerCase());
+  }
+
+
+  Future loginRequest(username, password, remember) async {
+    _isLoading = true;
+    notifyListeners();
+
+    var _loginAuth = await LoginConveyor.getInstance().login(username, password);
+
+    if (_loginAuth != null) {
+      _isSuccess = true;
+      isUserLoggedIn = true;
+      _isLoading = false;
+
+      notifyListeners();
+    } else {
+      _isSuccess = false;
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void updateShowPassword({bool value = false}) {
+    showPassword = value;
+    notifyListeners();
+  }
+}
---
+++ lib/conveyors/login_conveyor.dart
@@ -0,0 +1,34 @@
+
+import 'dart:convert';
+import '../conveyor.dart';
+
+class LoginConveyor extends Conveyor{
+
+static LoginConveyor? instance;
+
+static LoginConveyor getInstance() {
+return instance ?? LoginConveyor();
+}
+
+Future<Future> login(String username, String password) async {
+  String _username = username;
+  Future<dynamic> responseAuth = sendPost(
+      endpointPath: 'login',
+        requestBody: json.encode({
+          'grant_type': 'password',
+          'username': _username,
+          'password': password,
+        }));
+  return responseAuth;
+/*  if (responseAuth != null*//* && responseAuth['statuscode'] == 200*//*) {
+    return LoginAuth.fromJson(responseAuth as Future<dynamic>);
+  }
+  return null;*/
+}
+
+  @override
+  String? getBlueprintName() => null;
+
+  @override
+  createObject(Map<String, dynamic>? input) => null;
+}
