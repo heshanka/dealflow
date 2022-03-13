cb526bab6a0c432eb2905ef60d986bed58b1c3e9
heshanka <info@heshanka.com>
2022-03-13 19:33:30 +0300

    Added creator conveyor which handles http requests related to creators/users
    not finalized yet

---
+++ lib/conveyors/creator_conveyor.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import '../conveyor.dart';
+import '../models/creator.dart';
+
+class CreatorConveyor extends Conveyor<Creator>{
+
+  static CreatorConveyor? instance;
+
+  static CreatorConveyor? getInstance() {
+    return instance ?? CreatorConveyor();
+  }
+
+  @override
+  Creator createObject(Map<String, dynamic>? input) {
+    return Creator.fromJson(input!);
+  }
+
+  @override
+  String getBlueprintName() {
+    return ("users");
+  }
+
+  Future<List<Creator>?> getCreatorList({Map<String, String>? headers, int? id} ) async {
+    Response? response = await sendGet();
+    print("RESPONSE $response");
+    List<Creator> returnList = [];
+    for (var item in json.decode(response!.body)) {
+      returnList.add(Creator.fromJson(item as Map<String, dynamic>));
+    }
+    return response != null && response.statusCode == 200
+        ? returnList
+        : null;
+  }
+}
